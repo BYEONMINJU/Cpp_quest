7fa05dc36cc47b9d13cf7d726502096b32527352
BYEONMINJU <minju-8206@naver.com>
2025-05-21 23:30:10 +0900

    20250521

---
+++ ch 9/9-7/9-7/9-7.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Shape {
+protected:
+	string name;
+	int width, height;
+public:
+	Shape(string n = "", int w = 0, int h = 0) { name = n; width = w; height = h; }
+	virtual double getArea() { return 0; }
+	string getName() { return name; }
+};
+
+class Oval :public Shape {
+public:
+	Oval(){}
+	Oval(string name,int w,int h): Shape(name,w,h){}
+	double getArea() { return width * height; }
+};
+
+class Triangular :public Shape {
+public:
+	Triangular(){}
+	Triangular(string name, int w, int h) : Shape(name, w, h) {}
+	double getArea() { return width * height/2; }
+};
+
+class Rect :public Shape {
+public:
+	Rect(){}
+	Rect(string name, int w, int h) : Shape(name, w, h) {}
+	double getArea() { return width * height; }
+};
+
+int main() {
+	Shape* p[3];
+	p[0] = new Oval("ºó´ë¶±", 10, 20);
+	p[1] = new Rect("Âû¶±", 30, 40);
+	p[2] = new Triangular("Åä½ºÆ®", 30, 40);
+
+	for (int i = 0; i < 3; i++) {
+		cout << p[i]->getName() << " ³ÐÀÌ´Â " << p[i]->getArea() << endl;
+	}
+	for (int i = 0; i < 3; i++) delete p[i];
+	return 0;
+}
